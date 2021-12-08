17d35998a8f96d29230b456ddc9b90a08e44c38d
Joakim Hall <joakim.hall1@gmail.com>
2021-12-08 21:55:52 +0100

    Day One

--- src/main/kotlin/days/Day1.kt
+++ src/main/kotlin/days/Day1.kt
@@ -1,17 +1,17 @@
 package days
 
 class Day1 : Day(1) {
+    // https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/windowed.html
+    private fun withStep (size: Int): List<List<Int>> {
+        return inputList.map{ it.toInt() }.windowed(size)
+    }
+
 
     override fun partOne(): Any {
-        return inputList.take(2)
-            .map { it.uppercase() }
-            .joinToString(" ")
+        return withStep(2).count { (first, second) -> first < second}
     }
 
     override fun partTwo(): Any {
-        return inputString.split("\n")
-            .filterNot { it.isEmpty() }
-            .map { it.uppercase() }
-            .last()
+        return withStep(4).count { it[0] < it[3] }
     }
 }
